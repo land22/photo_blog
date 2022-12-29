5809608d43302bfe61644dbe191c18b09a3c7a9b
landry wabo <landrywabo8@gmail.com>
2022-12-29 15:10:40 +0100

    ajout de la fonctionnalité de déconnection et de redirection d'un utilisateur qui essaye d'acceder à une page s'il n'est pas connecté

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import blog.views
+
+import authentication.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', authentication.views.login_page, name='login'),
+    path('logout/', authentication.views.logout_user, name='logout'),
+    path('home/', blog.views.home, name='home'),
 ]
